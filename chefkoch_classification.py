951cd17dc0f184de6c910cedb9a040d05e925af4
Anna P <polizzoa@uni-bremen.de>
2017-06-30 15:57:29 +0200

    classification class updated

--- chefkoch_classification.py
+++ chefkoch_classification.py
@@ -11,82 +11,61 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
 from sklearn import svm
-from sklearn.cross_validation import train_test_split
+
+
+recipeMatrix = pd.read_csv('recipeMatrix.csv', encoding = 'utf-8')
+ratingsDF = pd.read_csv('ratingsDF.csv', encoding = 'utf-8')
 
 
 
-def read_datasets():
+# it reduces the dimensionality of both datasets and returns them
+def reduce_datasets():
 	
 	# creates a numpy array with recipes out of the dataframe 
-	recipes_dataset = reduce_dimensionality(pd.read_csv('recipeMatrix.csv', encoding = 'utf-8'))
+	recipes_dataset = reduce_dimensionality(recipeMatrix)
 	# creates a numpy array with ratings out of the dataframe
-	ratings_dataset = reduce_dimensionality(pd.read_csv('ratingsDF.csv', encoding = 'utf-8'))
+	ratings_dataset = reduce_dimensionality(ratingsDF)
 	return recipes_dataset,ratings_dataset
 
 
-
+# it uses pca for dimensionality reduction
 def reduce_dimensionality(dataset):
 
 	pca = PCA()
 	ds = pca.fit_transform(dataset)
 	return ds
 
-
+# it splits the dataset into train, validation and test sets and returns their arrays
 def split_ds(dataset):
 
 	train, val, test = np.split(dataset.sample(frac=1), [int(.6*len(dataset)), int(.8*len(dataset))])
-	return train, val, test
+	return train.as_matrix(), val.as_matrix(), test.as_matrix()
 
 
-
-
-
+# it returns arrays for each of the recipe training, validation and test sets
 def split_recipeDS():
 
-	recipes_dataset = read_datasets()[0]
-	x_train, x_validate, x_test = split_ds(recipes_dataset)
+	recipes = reduce_datasets()[0]
+	x_train, x_validate, x_test = split_ds(recipes)
 	return x_train, x_validate, x_test
 
 
 
-
+# it returns arrays for each of the ratings training, validation and test sets
 def split_ratingsDS():
 
-	ratings_dataset = read_datasets()[1]
-	y_train, y_validate, y_test = split_ds(ratings_dataset)
+	ratings= reduce_datasets()[1]
+	y_train, y_validate, y_test = split_ds(ratings)
 	return y_train, y_validate, y_test
 
 
 
+# it creates and returns the svc model
+def create_svc():
 
-def recipesDS_toArray():
-
-	# it creates numpy arrays for the x data (recipes)
-	training_recipes, validation_recipes, test_recipes = x_train.as_matrix(), x_validate.as_matrix(), x_test.as_matrix() 
-	return training_recipes, validation_recipes, test_recipes
+	svc = svm.SVC(gamma=0.001, C=100., kernel='linear')
 
 
-
-
-def ratingsDS_toArray():
-
-	# it creates numpy arrays for the y data (ratings)
-	training_ratings, validation_ratings, test_ratings = y_train.as_matrix(), y_validate.as_matrix(), y_test.as_matrix()
-	return training_ratings, validation_ratings, test_ratings
-
-
-# not yet refactored
-
-
-
-# class labels = chefkoch sterne
-cat = [1,2,3,4,5]
-
-# this returns a shape
-classifier = svm.LinearSVC(decision_function_shape='ovr')
-
-# Fit the SVM model according to the given training data.
-classifier.fit(train, cat)
 
 
 
